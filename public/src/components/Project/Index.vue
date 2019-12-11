f6cd5df3cf4801a348b45ad0d25a3ac18c5d728b
Rem <dkushneriov@aurum-soft.com>
2019-12-11 12:51:27 +0200

    Frontend:     Creating "Graph" component     Creating default /project/index page

--- public/src/components/Project/Index.vue
+++ public/src/components/Project/Index.vue
@@ -1,11 +1,30 @@
-<template src="@/templates/project/view.html" />
+<template src="@/templates/project/index.html" />
 
 <script>
+    import Graph from "@/components/Graph";
+
 export default {
     name: 'Index',
+    components: { Graph },
     data () {
         return {
-            project: {}
+            projectData: [
+                [1, 2],
+                [1, 3],
+                [2, 4],
+                [2, 5],
+                [3, 5],
+                [3, 6],
+                [3, 7],
+                [4, 8],
+                [5, 8],
+                [6, 9],
+                [6, 10],
+                [7, 10],
+                [8, 11],
+                [9, 11],
+                [10, 11]
+            ]
         }
     }
 }
